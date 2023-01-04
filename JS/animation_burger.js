ae61945a826615d5dab9c340bf7bc3a79d24647f
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2023-01-04 19:30:34 +0100

    création de fonction js pour les animation

--- JS/animation_burger.js
+++ JS/animation_burger.js
@@ -1,3 +1,5 @@
+/// BURGER ///
+
 /* Sélection des éléments HTML */
 let hamburger = document.getElementById("hamburger");
 let burger = document.getElementById("burger");
@@ -9,3 +11,58 @@ hamburger.addEventListener("click", function (e) {
   burger.classList.toggle("open");
   ul.classList.toggle("open");
 });
+
+/// ANIMATION ///
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 170;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+function revealHR() {
+      var reveals = document.querySelectorAll(".revealHR");
+    
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 170;
+    
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+
+    function revealHL() {
+    var reveals = document.querySelectorAll(".revealHL");
+    
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 170;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+    window.addEventListener("scroll", revealHR);
+    window.addEventListener("scroll", revealHL);
+    window.addEventListener("scroll", reveal);
+
